29d277edd5a9a1d466840695e0d1aef8c150ce89
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-01-28 23:28:26 +0100

    Merge branch 'main' of https://github.com/Kseniia888/challenge_portfolio_uvakso

--- pages/match_form.py
+++ pages/match_form.py
@@ -4,13 +4,13 @@ class Match_form (BasePage):
    Input_my_team_xpath = "//input[@name='myTeam']"
    Input_enemy_team_team_xpath = "//input[@name='enemyTeam']"
    inscription_my_team_xpath =  "//div[1]/div/label"
-   match_at_home_radi_but_xpath = '// input[ @ value = "true"]'
-   match_out_home_radi_but_xpath = '// input[ @ value = "false"]'
-   button_submit_xpath = '//button[@type="submit"]'
-   button_clear_xpath = '//div[3]/button[2]'
-   date_input_xpath = '//input[@type="date"]'
-   Adding_match_player_abc_Makowski_xpath= '//form/div[1]/div/span'
-   lable_Raiting_xpath = '//div[13]/div/label'
+   match_at_home_radi_but_xpath = "// input[ @ value = 'true']"
+   match_out_home_radi_but_xpath = "// input[ @ value = 'false']"
+   button_submit_xpath = "//button[@type='submit']"
+   button_clear_xpath = "//div[3]/button[2]"
+   date_input_xpath = "//input[@type='date']"
+   Adding_match_player_abc_Makowski_xpath= "//form/div[1]/div/span"
+   lable_Raiting_xpath = "//div[13]/div/label"
 
 
 

762397d3a0fd3cd9d67eedabc9c697f1040d3a82
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-01-30 16:28:32 +0100

    Merge branch 'main' of https://github.com/Kseniia888/challenge_portfolio_uvakso

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,7 +1,27 @@
+import time
+
 from pages.base_page import BasePage
 
 
-class Dassboard(BasePage):
+class Dashboard(BasePage):
+    expected_title = "Scouts panel"
+    dashboard_url = "https://scouts-test.futbolkolektyw.pl/"
+
+    def titele_of_page (self):
+        time.sleep(5)
+        assert self.get_page_title(self.dashboard_url) == self.expected_title
+
+
+
+
+
+
+
+
+
+
+
+    '''
     Scouts_Panel_xpath = "//header/div/h6"
     Main_page_xpath = "//ul[1]/div[1]/div[2]/span"
     Players_xpath = "//ul[1]/div[2]/div[2]/span"
@@ -12,3 +32,10 @@ class Dassboard(BasePage):
     Last_creater_player_xpath = "//div/div/h6[1]"
     Activity_xpath = "//div[3]/div/div/h2"
     count_of_player_number_xpath = "//div[1]/div/div/b"
+
+    '''
+
+
+
+
+
--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -1,4 +1,5 @@
 import os
+import time
 import unittest
 from selenium import webdriver
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
@@ -16,9 +17,19 @@ class TestLoginPage(unittest.TestCase):
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
 
+    def test_login_into_the_system (self):
+        user_login = LoginPage(self.driver)
+        user_login.titele_of_page()
+        user_login.type_in_email('user07@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sing_in_button()
+        time.sleep(5)
 
 
-    @classmethod
+
+
+  '''
+  @classmethod
     def Test_Login_Page(self):
         user_login = LoginPage(self.driver)
         user_login.type_in_email('user07@getnada.com')
@@ -30,11 +41,15 @@ class TestLoginPage(unittest.TestCase):
     def Test_Click_Button(self):
         user_click = LoginPage(self.driver)
         user_click.click_on_the_button()
+  
+  '''
+
 
 
 
     @classmethod
     def tearDown(self):
         self.driver.quit()
+
 
     #unittest.main()56

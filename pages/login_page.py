389347a72762a3afc9f3933c2947e9f7e84ce46b
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-01-30 16:33:00 +0100

    Merge branch 'main' of https://github.com/Kseniia888/challenge_portfolio_uvakso

--- pages/login_page.py
+++ pages/login_page.py
@@ -18,7 +18,7 @@ class LoginPage(BasePage):
     def click_on_the_button(self):
         self.click_on_the_element(self.sign_in_button_xpath)
 
-    def titele_of_page(self):
+    def title_of_page(self):
         assert self.get_page_title(self.login_url) == self.expected_title
 
 

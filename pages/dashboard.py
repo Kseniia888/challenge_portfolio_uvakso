e60538c3d1d45d3ea1a1a8e1a34eadd5a961881d
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-02-05 14:11:51 +0100

    Merge branch 'main' of https://github.com/Kseniia888/challenge_portfolio_uvakso

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -7,8 +7,20 @@ class Dashboard(BasePage):
     expected_title = "Scouts panel"
     dashboard_url = "https://scouts-test.futbolkolektyw.pl/"
 
+
+    Scouts_Panel_xpath = "//header/div/h6"
+    Main_page_xpath = "//ul[1]/div[1]/div[2]/span"
+    Players_xpath = "//ul[1]/div[2]/div[2]/span"
+    Language_xpath = "//ul[2]/div[1]/div[2]/span"
+    Sing_out_xpath = "//ul[2]/div[2]/div[2]/span"
+    Dev_team_contact_xpath = "//a[contains(@href, '://')]"
+    Ania_Testerska1_link_xpath = "//div[3]/div/div/a[1]"
+    Last_creater_player_xpath = "//div/div/h6[1]"
+    Activity_xpath = "//div[3]/div/div/h2"
+    count_of_player_number_xpath = "//div[1]/div/div/b"
+
     def title_of_page(self):
-        time.sleep(4)
+        self.wait_for_element_to_be_clickable(self.Sing_out_xpath)
         assert self.get_page_title(self.dashboard_url) == self.expected_title
 
 
@@ -21,19 +33,9 @@ class Dashboard(BasePage):
 
 
 
-    '''
-    Scouts_Panel_xpath = "//header/div/h6"
-    Main_page_xpath = "//ul[1]/div[1]/div[2]/span"
-    Players_xpath = "//ul[1]/div[2]/div[2]/span"
-    Language_xpath = "//ul[2]/div[1]/div[2]/span"
-    Sing_out_xpath = "//ul[2]/div[2]/div[2]/span"
-    Dev_team_contact_xpath = "//a[contains(@href, '://')]"
-    Ania_Testerska1_link_xpath = "//div[3]/div/div/a[1]"
-    Last_creater_player_xpath = "//div/div/h6[1]"
-    Activity_xpath = "//div[3]/div/div/h2"
-    count_of_player_number_xpath = "//div[1]/div/div/b"
 
-    '''
+
+
 
 
 
--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -25,13 +25,13 @@ class TestLoginPage(unittest.TestCase):
     def test_login_into_the_system(self):
         user_login = LoginPage(self.driver)
         user_login.title_of_page()
-        base_page = BasePage(self.driver)
-        base_page.assert_element_text(self.driver, '//div/div[1]/h5', 'Scouts Panel')
         user_login.type_in_email('user07@getnada.com')
         user_login.type_in_password('Test-1234')
         user_login.click_on_the_button()
         dashboard_page = Dashboard(self.driver)
         dashboard_page.title_of_page()
+        base_page = BasePage(self.driver)
+        base_page.assert_element_text(self.driver, '//div/div[1]/h5', 'Scouts Panel')
 
 
 

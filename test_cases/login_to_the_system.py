f40cd0364f7482c598f798f8bec22bd99043f3b2
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-02-05 14:15:45 +0100

    Merge branch 'main' of https://github.com/Kseniia888/challenge_portfolio_uvakso

--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -28,10 +28,11 @@ class TestLoginPage(unittest.TestCase):
         user_login.type_in_email('user07@getnada.com')
         user_login.type_in_password('Test-1234')
         user_login.click_on_the_button()
-        dashboard_page = Dashboard(self.driver)
-        dashboard_page.title_of_page()
         base_page = BasePage(self.driver)
         base_page.assert_element_text(self.driver, '//div/div[1]/h5', 'Scouts Panel')
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
+
 
 
 

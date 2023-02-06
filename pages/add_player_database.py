c81c6d87b303be5029fd0c8ee4dc743fc6c53da5
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-02-06 19:23:42 +0100

    Merge branch 'main' of https://github.com/Kseniia888/challenge_portfolio_uvakso

--- pages/add_player_database.py
+++ pages/add_player_database.py
@@ -18,6 +18,17 @@ class AddPlayerToDataBase(BasePage):
     weight_xpath = "//input[@name = weight]"
     height_xpath = "//input[@name = 'height']"
     age_xpath = "//input[@name = 'age']"
+    leg_xpath = "//*[@id = 'mui-component-select-leg']"
+    club_xpath = "//input[@name='club'"
+    level_xpath = "//input[@name = 'level']"
+    mainPosition_xpath = "//input[@name = 'mainPosition']"
+    secondPosition_xpath = "//input[@name = 'secondPosition']"
+    district_xpath = "//*[@id = 'mui-component-select-district']"
+    achievements_xpath = "//input[@name = 'achievements']"
+    webLaczy_xpath = "//input[@name = 'webLaczy']"
+    web90_xpath = "//input[@name = 'web90']"
+    webFB_xpath = "//input[@name = 'webFB']"
+
 
 
 

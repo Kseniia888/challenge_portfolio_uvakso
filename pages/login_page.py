1fe461256c5dd0d551d3aafc77af5c365736b6ab
Kseniia888 <122262452+Kseniia888@users.noreply.github.com>
2023-02-07 22:36:22 +0100

    Merge branch 'main' of https://github.com/Kseniia888/challenge_portfolio_uvakso

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,6 +14,7 @@ class LoginPage(BasePage):
     expected_reminder_title = 'Remind password'
     warning_sign_xpath = "//div[3]/span"
     expected_warning = 'Identifier or password invalid.'
+    language_changer_xpath = "//ul[2]/div[1]/div[2]/span"
 
     #   "//a/text()"
 
@@ -25,6 +26,9 @@ class LoginPage(BasePage):
 
     def click_on_the_button(self):
         self.click_on_the_element(self.sign_in_button_xpath)
+
+    def click_on_the_language(self):
+        self.click_on_the_element(self.language_changer_xpath)
 
     def title_of_page(self):
         assert self.get_page_title(self.login_url) == self.expected_title
---
+++ test_cases/change_language.py
@@ -0,0 +1,25 @@
+import os
+import time
+import unittest
+
+from selenium import webdriver
+
+from test_cases.login_to_the_system import TestLoginPage
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class ChangeLanguage(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/login')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_add_player_to_data(self):
+        TestLoginPage.test_login_into_the_system(self)
+        chang_language = LoginPage(self.driver)
+        time.sleep(3)
+        chang_language.click_on_the_language()
